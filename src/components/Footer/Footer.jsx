4dbe04724a294a8c35db52b676484acc4d13bf98
jamesjonathantossou-ayayi <jonathanelie007@gmail.com>
2026-01-05 22:02:04 +0000

    Update React app UI and logic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Jonathan Dev</p>
+      <p>&copy; 2025 JJ technologies</p>
     </footer>
   );
 }
